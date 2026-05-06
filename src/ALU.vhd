bb24c2b38e77d4bb5e59a9fcf0060fd038a6529b
jonathanhammond2 <82597323+jonathanhammond2@users.noreply.github.com>
2026-05-05 23:54:38 -0600

    alu still not working

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -47,34 +47,60 @@ signal w_res : signed(7 downto 0);
 
 begin
     process(i_A, i_B, i_op)
-    variable v_res : signed(7 downto 0);
+    variable v_res : unsigned(8 downto 0);
+    variable v_A, v_B : unsigned(8 downto 0);
+    
     begin 
+        v_A := unsigned('0' & i_A);
+        v_B := unsigned('0' & i_B);
+        
+    
         case i_op is
             when "000" => --addiiton
-                v_res := signed(i_A) + signed(i_B);
+                v_res := v_A + v_B;
             when "001" => --subtraction
-                 v_res := signed(i_A) - signed(i_B);
+                 v_res := v_A + unsigned(not ('0' & i_B)) + 1;
             when "010" => --bitwise AND
-                 v_res := signed(i_A and i_B);
+                 v_res := '0' & (unsigned(i_A) and unsigned(i_B));
             when "011" => --bitwise OR
-                 v_res := signed(i_A or i_B);
+                 v_res := '0' & (unsigned(i_A) or unsigned(i_B));
             when others => --default
                 v_res := (others => '0');
         end case;
-        w_res <= v_res;
+        o_result <= std_logic_vector(v_res(7 downto 0));
+        o_flags(3) <= v_res(7); --N
+        if v_res(7 downto 0) = x"00" then o_flags(2) <= '1';
+        else o_flags(2) <= '0';
+        end if;
+        
+        if i_op = "000" or i_op = "001" then
+            o_flags(1) <= v_res(8);
+        else
+            o_flags(1) <= '0';
+        end if;
+
+        -- V: Overflow flag (Signed math error)
+        if i_op = "000" then -- Addition Overflow
+            o_flags(0) <= (i_A(7) xnor i_B(7)) and (i_A(7) xor v_res(7));
+        elsif i_op = "001" then -- Subtraction Overflow
+            o_flags(0) <= (i_A(7) xor i_B(7)) and (i_A(7) xor v_res(7));
+        else
+            o_flags(0) <= '0';
+        end if;
+
     end process;
     
-    o_result <= std_logic_vector(w_res(7 downto 0));
+--    o_result <= std_logic_vector(w_res(7 downto 0));
     
     --flag logic:
     
-    o_flags(3) <= std_logic(w_res(7)); --negative flag is MSB of result N
+--    o_flags(3) <= std_logic(w_res(7)); --negative flag is MSB of result N
     
-    o_flags(2) <= '1' when (w_res = 0) else '0'; --Z
+--    o_flags(2) <= '1' when (w_res = 0) else '0'; --Z
     
-    o_flags(1) <= '1' when unsigned(i_A) + unsigned(i_B) > 255 else '0'; --C
+--    o_flags(1) <= '1' when unsigned(i_A) + unsigned(i_B) > 255 else '0'; --C
     
-    o_flags(0) <= (i_A(7) xnor i_B(7)) and (i_A(7) xor w_res(7)); --V
+--    o_flags(0) <= (i_A(7) xnor i_B(7)) and (i_A(7) xor w_res(7)); --V
 
 
     
